5f431e2718571426da5b13ea89a96b0d1f596046
Alex-Redwood <19132@wc.school.nz>
2023-08-03 12:11:26 +1200

    2/08/2023
    Score is working, not editable by player yet

--- DTM Main Project/Assets/Scripts/ScoreManager.cs
+++ DTM Main Project/Assets/Scripts/ScoreManager.cs
@@ -9,11 +9,12 @@ public class ScoreManager : MonoBehaviour
     // General score controlers
     public float scoreTimer = 0.0f;
     public bool offBeat = true;
-    public int scorePos = 0;
-    public int scoreLen = 4;
-    public int BPM = 90;
+    public int beatNum = 0; // the INDEX of the note, so starts at 0
+    public int scoreLen = 4; 
+    public int BPM = 60;
     public GameObject player;
     
+
     // score display variables
     public Image[] scoreMarkers;
     public GameObject scoreTracker;
@@ -33,38 +34,60 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scoreList.Add("false");
-        scoreList.Add("false");
-        scoreList.Add("true");
-        scoreList.Add("false");
+        scoreList.Add("empty");
+        scoreList.Add("bullet1");
+        scoreList.Add("empty");
+        scoreList.Add("bullet1");
 
 
         playerAudio = GetComponent<AudioSource>();
 
         // Gets width of the score bar
         scoreBarWidth = scoreBar.GetComponent<RectTransform>().sizeDelta.x;
+        UpdateScore();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Adds time to score timer
-        scoreTimer += Time.deltaTime;
+        scoreTimer += Time.deltaTime * BPM/60;
         scoreTracker.GetComponent<RectTransform>().anchoredPosition = new Vector2 ((scoreTimer * scoreBarWidth)/scoreLen-scoreBarWidth/2, scoreTracker.GetComponent<RectTransform>().anchoredPosition.y);
                                                                               
-        // Resets the score timer if over the total time length of the bar seconds
-        if (scoreTimer > scoreLen) {
+        // Resets the score timers if over the total time length of the bar seconds
+        if (scoreTimer >= scoreLen) {
             scoreTimer = 0;
+            beatNum = 0;
         }
 
-        Debug.Log(scoreTimer);
 
         
-        if ((0 <= scoreTimer - Mathf.Floor(scoreTimer)) && (scoreTimer - Mathf.Floor(scoreTimer)) <= 0.5 && !offBeat) { // An elaborate if else expresion for 0 < x < 1
+        if ((0 <= scoreTimer - Mathf.Floor(scoreTimer)) && (scoreTimer - Mathf.Floor(scoreTimer)) <= 0.5 && !offBeat) { // An elaborate if else expresion for 0 < x < 0.5
             offBeat = true;
         } else if ((0.5 < scoreTimer - Mathf.Floor(scoreTimer)) && (scoreTimer - Mathf.Floor(scoreTimer)) < 1 && offBeat){
             offBeat = false;
+            OnNote();
+            beatNum += 1;
+        }
+    }
+
+    void UpdateScore()
+    {
+        for (int i=0; i < scoreLen; i++) {
+            Debug.Log(scoreList[i]);
+            if (scoreList[i] is "empty") {
+                scoreMarkers[i].enabled = false;
+            } else {
+                scoreMarkers[i].enabled = true;
+            }
+        }
+    }
+
+    void OnNote() 
+    {
+        if (scoreList[beatNum] is not "empty") {
             player.GetComponent<PlayerController>().ShootProjectile();
+            playerAudio.PlayOneShot(beat);
         }
     }
 }
